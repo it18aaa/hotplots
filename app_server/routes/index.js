cb0c83e0a610e301a483604707858298df5b0e06
Ian Thomson <i.thomson@ljmu.ac.uk>
2019-12-01 13:29:06 +0000

    Added Ctrl Main.  Added Bootstrap to project

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const ctrlMain = require('../controllers/main');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hot Plots!' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Hot Plots!' });
+// });
+
+// const homepageController = (req, res) => {
+//   res.render('index', {title: 'Hot Plots!'});  
+// };
+
+router.get('/', ctrlMain.index);
 
 module.exports = router;
