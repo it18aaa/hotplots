653060e4994c809e8f14797370c775bba2a2728f
Ian Thomson <i.thomson@ljmu.ac.uk>
2019-12-11 22:59:04 +0000

    api: added controller method to create article

--- api/controllers/articles.js
+++ api/controllers/articles.js
@@ -10,17 +10,39 @@ var sendJsonResponse = (res, status, content) => {
 
 module.exports.fetchById = (req, res) => {
     
-    Article
-        .findById(req.params.id)
+    Article.findById(req.params.id)
         .exec((err, article) => {
             sendJsonResponse(res, 200, article);
         });
-    //sendJsonResponse(res, 200, {"status" : "success"});
+    
+   //sendJsonResponse(res, 200, {"status" : "success"});
 }
+
 module.exports.articleList = (req, res) => {
+    console.log('articleList');
     sendJsonResponse(res, 200, {"status" : "success"});
 }
 
 module.exports.articleCreate = (req, res) => {
-    sendJsonResponse(res, 200, {"status" : "success"});
-}
+   
+    
+    Article.create({
+        title: req.body.title,
+        author: req.body.author,
+        body: req.body.body
+    }, (err, article) => {    // callback
+        if(err) {
+            res
+                .status(400)
+                .json(err);
+        } else {
+            res
+                .status(201)
+                .json({"status" : "success"});
+        }
+    });
+    
+    //sendJsonResponse(res, 200, {"status" :"success"});
+
+    
+} 
--- api/routes/index.js
+++ api/routes/index.js
@@ -4,8 +4,8 @@ var ctrlArticle = require('../controllers/articles');
 var router = express.Router();
 console.log('api-router');
 // map controllers to api routes
-router.get('/article/:id', ctrlArticle.fetchById);
 router.get('/article/list', ctrlArticle.articleList);
+router.get('/article/:id', ctrlArticle.fetchById);
 router.post('/article/create', ctrlArticle.articleCreate);
 
 module.exports = router;
